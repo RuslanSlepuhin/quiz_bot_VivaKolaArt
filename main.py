3ae0e99fe24df590639e2a939c086dadd977e304
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2023-10-19 10:18:36 +0300

    18.10.23

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import configparser
 from Bot_view.bot_polling import BotHandlers
 
 config = configparser.ConfigParser()
-config.read("./settings/config.ini")
+try:
+    config.read("./settings/config.ini")
+except:
+    config.read("./../settings/config.ini")
 token = config['Bot']['token']
 
 if __name__ == '__main__':
